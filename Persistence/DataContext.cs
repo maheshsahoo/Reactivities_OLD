5c2f97f7b6ec25a93fc0f67d9d4e5ab8ac00ee70
maheshsahoo <62804423+maheshsahoo@users.noreply.github.com>
2020-08-12 21:54:49 +0530

    Update DataContext.cs

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -18,7 +18,7 @@ namespace Persistence
         {
             builder.Entity<Value>()
             .HasData(
-                new Value{Id=1, Name="Value 101"},
+                new Value{Id=1, Name="Value 1013"},
                 new Value{Id=2, Name="Value 102"},
                 new Value{Id=3, Name="Value 103"}
             );
